b7750b6383911d9b21226134825326d8fd72b0f3
Hemal <88051836+hemal-divyang@users.noreply.github.com>
2023-07-21 22:31:16 +0530

    Add files via upload

---
+++ destructuring.js
@@ -0,0 +1,69 @@
+// assigning object attributes to variables
+const person = {
+    name: 'Sara',
+    age: 25,
+    gender: 'female'    
+}
+
+// destructuring assignment
+let { name, age, gender } = person;
+
+console.log(name); // Sara
+console.log(age); // 25
+console.log(gender); // female
+
+
+const person = {
+    name: 'Sara',
+    age: 25,
+    gender: 'female'    
+}
+
+// destructuring assignment
+// using different variable names
+let { name: name1, age: age1, gender:gender1 } = person;
+
+console.log(name1); // Sara
+console.log(age1); // 25
+console.log(gender1); // female
+
+
+// array destructuring
+const arrValue = ['one', 'two', 'three'];
+
+// destructuring assignment in arrays
+const [x, y, z] = arrValue;
+
+console.log(x); // one
+console.log(y); // two
+console.log(z); // three
+
+//array default value 
+let arrValue = [10];
+
+// assigning default value 5 and 7
+let [x = 5,  y = 7] = arrValue;
+
+console.log(x); // 10
+console.log(y); // 7
+
+//assign remaining value 
+const arrValue = ['one', 'two', 'three', 'four'];
+
+// destructuring assignment in arrays
+// assigning remaining elements to y
+const [x, ...y] = arrValue;
+
+console.log(x); // one
+console.log(y); // ["two", "three", "four"]
+
+//nested destructuring
+// nested array elements
+const arrValue = ['one', ['two', 'three']];
+
+// nested destructuring assignment in arrays
+const [x, [y, z]] = arrValue;
+
+console.log(x); // one
+console.log(y); // two
+console.log(z); // three
