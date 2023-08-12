214c73382e1cff4445262bdf426f37091eb8f1d6
Hemal <88051836+hemal-divyang@users.noreply.github.com>
2023-08-12 10:06:00 +0530

    Update call-back.js

--- call-back.js
+++ call-back.js
@@ -1,3 +1,4 @@
+//Any function that is passed as an argument to another function so that it can be executed in that other function is called as a callback function.
 // function
 function greet(name, callback) {
     console.log('Hi' + ' ' + name);
