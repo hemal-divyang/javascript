3424273ec3ecfcbd38305c50f47ec4d60e09666f
Hemal <88051836+hemal-divyang@users.noreply.github.com>
2023-08-16 11:17:45 +0530

    Update encapsulation.js

---
+++ encapsulation.js
@@ -0,0 +1,4 @@
+Encapsulation can be defined as 'the packing of data and functions into one component'.
+Packing, which is also known as bundling, grouping and binding,
+simply means to bring together data and the methods which operate on data.
+The component can be a function, a class or an object.
