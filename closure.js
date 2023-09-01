bd6d4b189beb99a6929d2755c8a884df628acc3d
Hemal <88051836+hemal-divyang@users.noreply.github.com>
2023-09-01 21:45:54 +0530

    Update closure.js

--- closure.js
+++ closure.js
@@ -1,4 +1,7 @@
-//closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment).
+//closure is the combination of a function bundled together (enclosed) with references to its surrounding state(the lexical environment).
+//A closure is a feature of JavaScript that allows inner functions to access the outer scope of a function.
+//Closure helps in binding a function to its outer boundary and is created automatically whenever a function is created.
+//A block is also treated as a scope since ES6.
 function greet(name) {
     function displayName() {
         console.log('Hi' + ' ' + name);
