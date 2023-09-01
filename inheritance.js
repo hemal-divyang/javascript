38dc479ea226ebefb5fbbd29c7ae6c922f5b6112
Hemal <88051836+hemal-divyang@users.noreply.github.com>
2023-09-01 21:57:42 +0530

    Update inheritance.js

--- inheritance.js
+++ inheritance.js
@@ -1,3 +1,4 @@
+// Inherit means receive from previous holder
 //Inheritance in javascript is used to inherit attributes and methods of a class. Classes are inherited using the extends keyword.
 //The class that is being inherited is known as the parent class, and the class that is inheriting the parent class is known as the child class.
 class employee{
