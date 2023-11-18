ce6ce4d8295e13d8e57e9a57471330595a0d4d4f
Hemal <88051836+hemal-divyang@users.noreply.github.com>
2023-11-18 11:33:27 -0500

    Update inheritance.js

--- inheritance.js
+++ inheritance.js
@@ -1,6 +1,8 @@
 // Inherit means receive from previous holder
 //Inheritance in javascript is used to inherit attributes and methods of a class. Classes are inherited using the extends keyword.
 //The class that is being inherited is known as the parent class, and the class that is inheriting the parent class is known as the child class.
+
+//class inheritance
 class employee{
     constructor(name){
         this.name = name;
@@ -13,3 +15,20 @@ class employee{
 class User extends employee{}
 let empUser = new User("Hemal")
 console.log(empUser.greet())
+
+//property inheritance
+
+function Alien(name, phrase) {
+    this.name = name
+    this.phrase = phrase
+    this.species = "alien"
+}
+
+Alien.prototype.fly = () => console.log("Zzzzzziiiiiinnnnnggggg!!")
+Alien.prototype.sayPhrase = () => console.log(this.phrase)
+
+const alien1 = new Alien("Ali", "I'm Ali the alien!")
+
+console.log(alien1.name) // output "Ali"
+console.log(alien1.phrase) // output "I'm Ali the alien!"
+alien1.fly() // output "Zzzzzziiiiiinnnnnggggg"
