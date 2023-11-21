254bd83095700882c83243072f0f1c109ff2a204
Hemal <88051836+hemal-divyang@users.noreply.github.com>
2023-11-21 00:00:05 -0500

    Update call-apply-bind.js

--- call-apply-bind.js
+++ call-apply-bind.js
@@ -1,6 +1,6 @@
 
 //The call, bind and apply methods can be used to set the this keyword independent of how a function is called.
-//The bind method creates a copy of the function and sets the this keyword, 
+//The bind method creates a copy of the function and sets the this keyword, With the bind() method, an object can borrow a method from another object.
 //while the call and apply methods sets the this keyword and calls the function immediately.
 
 //CALL,APPLY,BIND
