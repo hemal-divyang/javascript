02671e4fccc5e975ed6d94963a3ee30cdd5807dc
Hemal <88051836+hemal-divyang@users.noreply.github.com>
2023-11-21 00:35:53 -0500

    Update var-let-const.js

--- var-let-const.js
+++ var-let-const.js
@@ -1,3 +1,15 @@
+When to Use var, let, or const?
+1. Always declare variables
+
+2. Always use const if the value should not be changed
+
+3. Always use const if the type should not be changed (Arrays and Objects)
+
+4. Only use let if you can't use const
+
+5. Only use var if you MUST support old browsers.
+
+ 
 //Function Scope: Visibility is limited to the function.
 // var declarations are function scoped.
  function myFn() {
