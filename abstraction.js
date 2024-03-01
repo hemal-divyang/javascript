369afd674bf1703f35bee54b0ab88decc5764a09
Hemal <88051836+hemal-divyang@users.noreply.github.com>
2024-02-29 22:39:16 -0500

    Update abstraction.js

--- abstraction.js
+++ abstraction.js
@@ -1,5 +1,6 @@
 //An abstraction is a way of hiding the implementation details and showing only the functionality to the users.
 //In other words, it ignores the irrelevant details and shows only the required one.
+// link to check video : https://www.youtube.com/watch?v=jM0WcyQWMSM&t=481s
 function Employee(name, sal){
     this.name = name;
   this.sal = sal;
