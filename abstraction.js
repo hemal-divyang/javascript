b5d613609a92cdda8b0684ae8617d228f4d783e6
Hemal <88051836+hemal-divyang@users.noreply.github.com>
2024-09-24 11:05:29 +0530

    Update abstraction.js

--- abstraction.js
+++ abstraction.js
@@ -2,18 +2,15 @@
 //In other words, it ignores the irrelevant details and shows only the required one.
 // link to check video : https://www.youtube.com/watch?v=jM0WcyQWMSM&t=481s
 function Employee(name, sal){
-    this.name = name;
+  this.name = name;
   this.sal = sal;
-
   let bonus = 2000;
-
   let calSal = function(){
       let finalSal = this.sal + bonus;
     return finalSal
   }
   this.getEmpDet = function(){
     return calSal.call(this);
-
   }
 }
 let emp = new Employee("Hemal", 3000)
